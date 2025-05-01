95f3adf14af8badf46dba366340135edaabd2448
febriLindung <febriperlindungan@student.uir.ac.id>
2025-05-01 16:44:51 +0700

    addItem

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,24 @@ const items = ref([
     done: false
   }
 ])
+
+const newItem = ref('')
+
+const addItem = () => {
+  items.value.push({
+    id : items.value.length + 1,
+    text: newItem.value,
+    done: false
+  })
+  newItem.value = ''
+}
 </script>
 
 <template>
+
+<input type="text" v-model="newItem" @keyup.enter="addItem">
+<button @click="addItem">Add</button>
+
 <ul>
   <li v-for="item in items" :key="item.text">
     {{ item.text }} : {{ item.done }}
