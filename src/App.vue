74548aee5a16531e3bccc265e6905061dd6684dc
febriLindung <febriperlindungan@student.uir.ac.id>
2025-05-01 16:55:32 +0700

    filteredItems

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const items = ref([])
-
 const newItem = ref('')
+const filter = ref('all')
 
 const addItem = () => {
   items.value.push({
@@ -22,15 +22,31 @@ const toggleDone = (item) => {
   item.done == !item.done
   console.log(item.done)
 }
+
+const filteredItems = computed(() => {
+  if (filter.value === 'all') {
+    return items.value
+  } else if (filter.value === 'done') {
+    return items.value.filter(item => item.done)
+  } else {
+    return items.value.filter(item => !item.done)
+  }
+})
+
 </script>
 
 <template>
 
   <input type="text" v-model="newItem" @keyup.enter="addItem">
   <button @click="addItem">Add</button>
+  <select v-model="filter">
+    <option value="all">All</option>
+    <option value="done">Done</option>
+    <option value="not done">Not Done</option>
+  </select>
 
   <ul>
-    <li v-for="item in items" :key="item.text">
+    <li v-for="item in filteredItems" :key="item.text">
       <input type="checkbox" v-model="item.done" @change="toggleDone(item)" :checked="item.done">
       {{ item.text }}
       <button @click="removeItem(item)">Remove</button>
