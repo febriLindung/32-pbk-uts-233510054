7fea5830cb5eadb5ee47ef9cf3a157d5779f2adf
febriLindung <febriperlindungan@student.uir.ac.id>
2025-05-01 16:50:50 +0700

    removeItem

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,37 @@
 <script setup>
 import { ref } from 'vue'
 
-const items = ref([
-  {
-    text: 'Makan',
-    done: false
-  },
-  {
-    text: 'Minum',
-    done: false
-  },
-  {
-    text: 'Main',
-    done: false
-  }
-])
+const items = ref([])
 
 const newItem = ref('')
 
 const addItem = () => {
   items.value.push({
-    id : items.value.length + 1,
+    id: items.value.length + 1,
     text: newItem.value,
     done: false
   })
   newItem.value = ''
 }
+
+const removeItem = (item) => {
+ items.value = items.value.filter(i => i.id !== item.id)
+}
+
+
 </script>
 
 <template>
 
-<input type="text" v-model="newItem" @keyup.enter="addItem">
-<button @click="addItem">Add</button>
+  <input type="text" v-model="newItem" @keyup.enter="addItem">
+  <button @click="addItem">Add</button>
 
-<ul>
-  <li v-for="item in items" :key="item.text">
-    {{ item.text }} : {{ item.done }}
-  </li>
-</ul>
+  <ul>
+    <li v-for="item in items" :key="item.text">
+      {{ item.text }}
+      <button @click="removeItem(item)">Remove</button>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
